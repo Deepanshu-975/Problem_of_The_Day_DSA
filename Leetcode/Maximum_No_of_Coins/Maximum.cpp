b1c92aa1dd481553218e7c2171c72b66b68426c5
Deepanshu Rathore <133076945+Deepanshurathore01@users.noreply.github.com>
2023-11-24 19:37:01 +0530

    Maximun no of coins piles

---
+++ Leetcode/Maximum_No_of_Coins/Maximum.cpp
@@ -0,0 +1,12 @@
+
+// tc : o(nlogn) 
+// sc : o(log n) or 
+int maxCoins(vector<int>& piles) {
+        sort(piles.begin(),piles.end());
+        int ans =0;
+        for(int i= piles.size() / 3;i <piles.size(); i +=2){
+            ans +=piles[i];
+        }
+
+        return ans;
+    }
