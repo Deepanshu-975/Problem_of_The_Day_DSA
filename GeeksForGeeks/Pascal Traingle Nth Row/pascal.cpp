1d198e1e7eb4ddb1654175c266535a730aa0579b
Deepanshu Rathore <133076945+Deepanshurathore01@users.noreply.github.com>
2023-11-24 19:35:42 +0530

    Pascal triangle print nthrow

---
+++ GeeksForGeeks/Pascal Traingle Nth Row/pascal.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+using namespace std;
+
+// optimized version
+//  tc : o(n) , sc :o(n)
+vector<long long> nthRowOfPascalTriangle(int n) {
+    int mod = 1e9 + 7;
+    vector<long long> ans(n, 0);
+    ans[0] = 1;
+
+    for (int i = 1; i < n; ++i) {
+        for (int j = i; j > 0; --j) {
+            ans[j] = (ans[j] + ans[j - 1]) % mod;
+        }
+    }
+
+    return ans;
+}
+
+
+
+//  tc:o(n^2)
+// sc :o(1)
+ vector<long long> nthRowOfPascalTriangle(int n) {
+        // code here
+      
+        int mod = 1e9 + 7;
+        vector<long long> ans(n, 0);
+        ans[0] = 1;
+        for(int i = 1; i < n; i++){
+            for(int j = i; j >= 0; j--){
+                ans[j+1] += ans[j];
+                ans[j+1] %= mod;
+            }
+        }
+        
+        return ans;
+    }
